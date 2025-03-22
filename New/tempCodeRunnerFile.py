16b71b09560ec20b4e0374b3ebf2aab87b432bf7
sharanyabaidya <146946622+sharanyabaidya@users.noreply.github.com>
2025-03-23 01:13:49 +0530

    Delete New directory

--- New/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import filedialog, messagebox, simpledialog
-import os
-import shutil
-import time
-import sqlite3
-import bcrypt
-import subprocess
-from tkinter import ttk 
